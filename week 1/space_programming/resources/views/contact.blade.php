894ff1f9eae5244bcab65605648be492479c7cb6
Othman <o.boudaati@gmail.com>
2026-05-22 12:17:47 +0200

    les 1 af

---
+++ week 1/space_programming/resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>mission contact</h3>
+    <p>lead contact</p>
+    <p>dr.clara</p>
+    
+</body>
+</html>
